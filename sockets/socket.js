045f5d4a245e0d77d9da1a3fea1af767495221e5
Daniela Fernàndez Varela <danny.fervar@gmail.com>
2023-02-24 15:45:29 -0600

    server finished

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,49 @@
 const { io } = require('../index');
 
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+
+bands.addBand(new Band('Deftones'));
+bands.addBand(new Band('Led Zepelin'));
+bands.addBand(new Band('Pink Floyd'));
+bands.addBand(new Band('Metallica'));
+
+
 //Mensajes del server
 io.on('connection', client => {
     console.log('Client connected');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => { console.log('Client disconnected') });
 
     client.on('mensaje', (payload) => {
         console.log('Mensaje', payload);
-
         io.emit('Mensaje', { admin: 'Nuevo Mensaje' });
     });
+
+    client.on('vote-band', (payload) => {
+        bands.voteBand(payload.id);
+        io.emit('active-bands', bands.getBands());
+    });
+
+    client.on('add-new-band', (payload) => {
+        const newBand = new Band(payload.name);
+        bands.addBand(newBand);
+        io.emit('active-bands', bands.getBands())
+    });
+
+    client.on('borrar-banda', (payload) => {
+        bands.deleteBand(payload.id);
+        io.emit('active-bands', bands.getBands())
+    });
+
+    // client.on('emitir-mensaje', (payload) => {
+    //     //io.emit('nuevo-mensaje', 'meh!'); //emite mensaje a todos.
+    //     client.broadcast.emit('nuevo-mensaje', payload); //emite el mensaje a todos menos a quien lo emitió.
+    //     console.log(payload);
+    // })
+
 });
